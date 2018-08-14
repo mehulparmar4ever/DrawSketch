babc3490f2eceeccdd02ad20d128f9ca61ef9cfd
infieldsEngineer <infieldsmarketing@gmail.com>
2018-08-14 15:21:46 +0530

    First commite
    Loaded code..

---
+++ Drawing/Controller/ColorListCell/ColorListCell.swift
@@ -0,0 +1,19 @@
+//
+//  ColorListCell.swift
+//  Infields
+//
+//  Created by Sooryen on 6/2/18.
+//  Copyright © 2018 . All rights reserved.
+//
+
+import UIKit
+
+class ColorListCell: UICollectionViewCell
+{
+    @IBOutlet weak var btnColor:UIButton!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+}
